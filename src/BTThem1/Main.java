b326ccf4feeda2b89de328ca34a22227b8dcd1ed
unknown <nguyenmai0102.meo@gmail.com>
2022-09-26 11:40:12 +0700

    noi dung

---
+++ src/BTThem1/Main.java
@@ -0,0 +1,17 @@
+package BTThem1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int dientich, chuvi;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input a ");
+     int  a = scanner.nextInt();
+     dientich = a*a;
+     chuvi = a*4;
+     System.out.println("dien tich hinh vuong là:"+ dientich);
+     System.out.println("chu vi hinh vuong la:" + chuvi);
+
+    }
+}
